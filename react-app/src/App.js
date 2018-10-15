049b88e362a1d20ba088a31c5e6a9342b5595e09
Stephan Griesel <sgriesel@gmail.com>
2018-10-15 20:07:32 +0200

    create react app & basic config

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import RockChart from './components/RockChart';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RockChart />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-app/src/components/RockChart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class RockChart extends Component {
+  render() {
+    return <h1>Chart</h1>
+  }
+}
+
+export default RockChart;
